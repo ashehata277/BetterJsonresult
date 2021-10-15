5182673183f3136e4e6e7a5b7352d5edc102ba3f
ashehata <ashehata277@gamil.com>
2021-10-15 15:52:55 +0200

    init

---
+++ Json/BetterJsonResult.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class BetterJsonResult : JsonResult
+    {
+        public Object Data { get; init; }
+        public BetterJsonResult(object value) : base(value)
+        {
+            Data = value;
+
+        }
+
+        public BetterJsonResult() : base(new object())
+        {
+
+        }
+
+
+
+        public JsonResult ExcuteResult()
+        {
+            var x = new BetterJsonResult(
+                Value = new
+                {
+                    ContentType = "",
+                    data = Data
+                });
+
+
+            return x;
+
+        }
+        public JsonResult ExcuteResultOk()
+        {
+            var x = new BetterJsonResult(
+                Value = new
+                {
+                    StatusCode=200,
+                    ContentType = "",
+                    data = Data
+                });
+
+
+            return x;
+
+        }
+        public JsonResult ExcuteResultBadRequest()
+        {
+            var x = new BetterJsonResult(
+                Value = new
+                {
+                    StatusCode=400,
+                    ContentType = "",
+                    data = Data
+                });
+
+
+            return x;
+
+        }
+    }
+
+
+
+
+    public class BetterJsonResult<T> : JsonResult
+    {
+        public T  Data 
+        {
+            get
+            {
+                return (T) Value;
+            }
+            init 
+            {
+                Value = (T) value;
+            } 
+        }
+        public BetterJsonResult(object value) : base(value)
+        {
+        }
+
+
+        public BetterJsonResult():base(new Object())
+        {
+
+        }
+
+        public JsonResult ExcuteResult(int? statuscode)
+        {
+            var x = new BetterJsonResult(
+                Value = new
+                {
+                    StatusCode = statuscode,
+                    ContentType = "",
+                    data = Data
+                });
+
+
+            return x;
+
+        }
+        public JsonResult ExcuteResultOk()
+        {
+            var x = new BetterJsonResult(
+                Value = new
+                {
+                    StatusCode = 200,
+                    ContentType = "",
+                    data = Data
+                });
+
+
+            return x;
+
+        }
+        public JsonResult ExcuteResultBadRequest()
+        {
+            var x = new BetterJsonResult(
+                Value = new
+                {
+                    StatusCode = 400,
+                    ContentType = "",
+                    data = Data
+                });
+
+
+            return x;
+
+        }
+
+    }
+
+
+    
+
+
+}
---
+++ Json/Controllers/Test.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json.Controllers
+{
+    [ApiController]
+    public class Test : Controller
+    {
+        public Test()
+        {
+
+        }
+        [HttpGet]
+        [Route("api/Test")]
+        public IActionResult TestApi()
+        {
+
+            return new BetterJsonResult<string>
+            {
+                Data = ""
+            }.ExcuteResultOk();
+        }
+
+
+        [HttpGet]
+        [Route("api/Test2")]
+        public IActionResult TestApi2()
+        {
+
+            return new BetterJsonResult<List<string>>
+            {
+                Data = new List<string>() 
+                {
+                    "Ahmed",
+                    "Mohamed"
+                }
+
+            }.ExcuteResult(200);
+        }
+
+
+      
+    }
+}
